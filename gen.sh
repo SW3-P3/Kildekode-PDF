c4b5b0db78979a8b486dfa029908fb68f99126e2
Troels Krøgh <troels.kroegh@gmail.com>
2015-05-26 12:22:03 +0200

    Fixed stuff for p4

--- gen.sh
+++ gen.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 find sourcefiles/*/* -maxdepth 0 -type d | cut -c13- > folder.txt
-while read folder           
+while read folder
 do
-	#Latex kapittel pr undermappe
-	echo "\\chapter{$folder}"
+    #Latex kapittel pr undermappe
+    echo "\\chapter{$folder}"
 
-	find ./sourcefiles/$folder -type f > sourcecode.txt
+    find ./sourcefiles/$folder -type f > sourcecode.txt
 
-	while read line           
-	do
-		#Latex header pr. file
-		echo "\\section{$(find $line | cut -c15- | sed 's/_/\\_/g')}"
-	    echo "\\begin{lstlisting}[caption=\"$(find $line | cut -c15- | sed 's/_/\\_/g')\"]"
-	    #Remove first 4 bytes (wrong characters??)
-	    cat $line | head -n1 | cut -b4-
-	    #Print everthing but the first line of file
-	    tail -n +2 "$line"
-	    echo -e "\n"  
-	    echo "\\end{lstlisting}"
-	    echo "\\clearpage"
-	done < sourcecode.txt
+    while read line           
+    do
+        #Latex header pr. file
+        echo "\\section{$(find $line | cut -c15- | sed 's/_/\\_/g')}"
+        echo "\\begin{lstlisting}[caption=\"$(find $line | cut -c15- | sed 's/_/\\_/g')\"]"
+        #Remove first 4 bytes (wrong characters??)
+        cat $line | head -n1 # | cut -b4-
+        #Print everthing but the first line of file
+        tail -n +2 "$line"
+        echo -e "\n"  
+        echo "\\end{lstlisting}"
+        echo "\\clearpage"
+    done < sourcecode.txt
 done < folder.txt
 
 
